5d497470e444ceeda82cb1cb9a1d65a939a81686
jingpengju <757769998@qq.com>
2024-10-26 21:46:14 +0800

    feature img loading

--- src/renderer/views/workOrderView/WorkOrderItem.vue
+++ src/renderer/views/workOrderView/WorkOrderItem.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 const props = withDefaults(defineProps<{ currentData: any | undefined, index: number, isLast: boolean }>(), {
     currentData: undefined,
     index: 0,
     isLast: true
 })
+const imageLoad = ref<boolean>(true)
+const closeLoad = () => (imageLoad.value = false)
 
 const handlerCurrentWorkOrder = () => {
     console.log(props.currentData, 9999)
@@ -13,7 +16,16 @@ const handlerCurrentWorkOrder = () => {
 <template>
     <div :class="{ paddingBottom: !isLast }" @click="handlerCurrentWorkOrder">
         <div :class="{ container: true, active: index===3 }">
-            <el-image class="container-image" src="https://picsum.photos/200/300" fit="cover" />
+            <el-image class="container-image" 
+                src="https://picsum.photos/200/300" 
+                @load="closeLoad"  
+                @error="closeLoad" 
+                fit="cover" 
+                v-loading="imageLoad">
+                <template #error>
+                    <i-ep-picture class="image-slot"/>
+                </template>
+            </el-image>
             <div class="container-right">
                 <aside>
                     <el-text class="title">Default</el-text>
