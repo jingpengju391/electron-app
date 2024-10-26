2222c957a31b00e829a34506821af3b6960b0bd8
jingpengju <757769998@qq.com>
2024-10-26 20:30:45 +0800

    feature net work info

--- src/renderer/views/components/Header/index.vue
+++ src/renderer/views/components/Header/index.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
-import { computed } from 'vue'; 'vue'
+import { computed, ref } from 'vue'; 'vue'
 import { useWindowStore } from '@stores'
 import { process } from '@hooks/api'
 import { SvgIcon } from '@components'
+import { getNetWorkInfo } from '@utils/netWork'
 
 const windowStore = useWindowStore()
 const minimize = computed(() => windowStore.$state.minimize)
 
-// const getSignalLevel = score => score === 0 ? 0 : Math.ceil(score / 20)
+const netInfo = ref<NetworkInformationMode>({ type: '无服务', rtt: 0, downlink: 0 })
+
+// const getSignalLevel = (score: number) => Math.min(score === 0 ? 0 : Math.ceil(score / 20), 5)
+const signal = computed<number>(() => {
+    if(!netInfo.value.rtt) return 0
+    return {
+        ['2g']: 3,
+        ['3g']: 4,
+        ['4g']: 5,
+        ['slow-2g']: 1,
+        ['无服务']: 0,
+    }[netInfo.value.type]
+})
+
 const handleMize = () => {
     minimize.value ? process.maximize() : process.restore()
     windowStore.updatedMinimize()
 }
+
+const updateWithNetWork = () => (netInfo.value = getNetWorkInfo())
+
+updateWithNetWork()
+window.addEventListener('online', updateWithNetWork)
+window.addEventListener('offline', updateWithNetWork)
+navigator.connection?.addEventListener('change', updateWithNetWork)
+
 
 </script>
 
@@ -19,16 +41,16 @@ const handleMize = () => {
     <div class="container">
         <i-ep-close class="margin close" color="#0d867f" @click="process.close" />
         <svg-icon class="margin" color="#0d867f" size="38" :name="minimize ? 'maximize' : 'restore'" @click="handleMize"/>
-        <svg-icon class="margin" color="#0d867f" name="suoxiao" @click="process.minimize"/>
+        <svg-icon class="margin minimize" color="#0d867f" name="suoxiao" @click="process.minimize"/>
         <!-- <div class="margin upload">
             <i class="upload-icon"></i>
             <span>14</span>
-        </div>
-        <svg-icon class="margin wifi" :name="`wifi_${ getSignalLevel(42) }`"/>
-        <div class="margin mobile">
-            <svg-icon :name="`mobile_${ getSignalLevel(52) }`"/>
-            <span>WAPI</span>
         </div> -->
+        <!-- <svg-icon class="margin wifi" :name="`wifi_${ getSignalLevel(42) }`"/> -->
+        <div class="margin mobile">
+            <svg-icon :name="`mobile_${ signal }`"/>
+            <span>{{ netInfo.type.toUpperCase() }}</span>
+        </div>
     </div>
 </template>
 
