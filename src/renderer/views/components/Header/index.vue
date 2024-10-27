cb50a3634da01603a5c67b5279a38f7eb636ec44
jingpengju <757769998@qq.com>
2024-10-27 14:07:58 +0800

    fix .d.ts

--- src/renderer/views/components/Header/index.vue
+++ src/renderer/views/components/Header/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-;('vue')
 import { useWindowStore } from '@stores'
 import { process } from '@hooks/api'
 import { SvgIcon } from '@components'
